804b70e6c284c4106b76950b8257e5bef8f637f9
Ruslan Radetskiy <russ2510@gmail.com>
2020-02-15 13:50:58 -0500

    fixed major mistake and is now working

--- WebLayer/App/App.py
+++ WebLayer/App/App.py
@@ -1,18 +1,23 @@
 from os import sys, path
+
+
+
 sys.path.append('/home/russ/recipeApp/recipeSaver/')
 
 import os
 from flask import Flask
 
-from WebLayer.Controllers.SearchRecipeController import RecipeController
+from WebLayer.Controllers.RecipeController import RecipeController
+from WebLayer.Controllers.RecipeSearchController import RecipeSearchController
 
 
 def create_app():
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = 'dude'
 	app.secret_key = os.urandom(24)
-	app.add_url_rule('/recipe/<recipeTitle>', view_func=RecipeController.as_view('recipesGet'), methods=['GET'])
-	app.add_url_rule('/recipe/', view_func=RecipeController.as_view('recipesPost'), methods=['POST'])
+	app.add_url_rule('/api/recipe/<recipeTitle>', view_func=RecipeController.as_view('recipesGet'), methods=['GET'])
+	app.add_url_rule('/api/recipe/search/<recipeTitle>', view_func=RecipeSearchController.as_view('recipeSearchGet'), methods=['GET'])
+	app.add_url_rule('/api/recipe/', view_func=RecipeController.as_view('recipesPost'), methods=['POST'])
 	return app
 
 app = create_app()
